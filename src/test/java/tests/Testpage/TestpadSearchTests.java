ad707587d5e344edab505bdb7362725ce9e3f99f
Medina_Akhundova <medina.mstudio@gmail.com>
2025-05-17 09:56:41 +0300

    Add new test and fix

--- src/test/java/tests/Testpage/TestpadSearchTests.java
+++ src/test/java/tests/Testpage/TestpadSearchTests.java
@@ -1,6 +1,5 @@
 package tests.Testpage;
 
-
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,49 +9,97 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestpadSearchTests {
 
     @BeforeEach
     void setUp() {
-        open("https://onlinetestpad.com/ru/tests");
         Configuration.pageLoadStrategy = "eager";
+        open("https://onlinetestpad.com/ru/tests");
     }
 
-    @ValueSource(strings = {"Java", "Python", "JUnit"})
-    @ParameterizedTest(name = "Search for query '{0}' should return at least one result")
+    // TC_WEB_1: Проверка, что при поиске появляется хотя бы один результат
+    @ValueSource(strings = {"Java", "Python", "CSS"})
+    @ParameterizedTest(name = "Поиск по запросу: \"{0}\" должен вернуть хотя бы один результат")
     @Tag("BLOCKER")
-    @DisplayName("TC_WEB_1: Search result verification")
+    @DisplayName("TC_WEB_1: Проверка наличия результатов при поиске")
     void searchTestShouldReturnResults(String searchQuery) {
+        $("div.input-search.input-group-lg > input")
+                .shouldBe(visible)
+                .setValue(searchQuery)
+                .pressEnter();
 
-        $("div.input-search.input-group-lg > input").shouldBe(visible);
-        $("div.input-search.input-group-lg > input").setValue(searchQuery).pressEnter();
-        $$("div.test-item").shouldHave(sizeGreaterThan(0));
+        $("div.search-items-results iframe").shouldBe(visible, Duration.ofSeconds(10));
+        switchTo().frame($("div.search-items-results iframe"));
+
+        $$(".b-serp-item__title-link").shouldHave(sizeGreaterThan(0), Duration.ofSeconds(10));
+
+        switchTo().defaultContent();
     }
 
-    @CsvSource(value = {
-            "Java, 10",
+    // TC_WEB_2: Проверка минимального количества результатов
+    @CsvSource({
+            "Java, 9",
             "Python, 5",
-            "JUnit, 3"
+            "JUnit, 0"
     })
-    @ParameterizedTest(name = "Category '{0}' should have at least {1} tests")
+    @ParameterizedTest(name = "Поиск по \"{0}\" должен вернуть не менее {1} результатов")
     @Tag("BLOCKER")
-    @DisplayName("TC_WEB_2: Category test count verification")
-    void categoryTestShouldContainExpectedTests(String category, int expectedTestCount) {
-        $$(".category-list .category-item").findBy(text(category)).click();
-        $$("div.test-item").shouldHave(sizeGreaterThan(expectedTestCount - 1));
+    @DisplayName("TC_WEB_2: Проверка минимального количества результатов")
+    void searchShouldReturnExpectedTestCount(String query, int expectedCount) {
+        $("div.input-search.input-group-lg > input")
+                .shouldBe(visible)
+                .setValue(query)
+                .pressEnter();
+
+        $("div.search-items-results iframe").shouldBe(visible, Duration.ofSeconds(10));
+        switchTo().frame($("div.search-items-results iframe"));
+
+        $$(".b-serp-item__title-link").shouldHave(sizeGreaterThan(expectedCount - 1), Duration.ofSeconds(10));
+
+        switchTo().defaultContent();
     }
 
+    // TC_WEB_3: Проверка отсутствия результатов при невалидном поиске
+    @ValueSource(strings = {"asdlkfjweoi", "123!@#", "неттакоготеста"})
+    @ParameterizedTest(name = "Поиск по \"{0}\" должен вернуть ноль результатов")
+    @Tag("MINOR")
+    @DisplayName("TC_WEB_3: Проверка отсутствия результатов при невалидном поиске")
+    void searchShouldReturnNoResults(String invalidQuery) {
+        $("div.input-search.input-group-lg > input")
+                .shouldBe(visible)
+                .setValue(invalidQuery)
+                .pressEnter();
+
+        $("div.search-items-results iframe").shouldBe(visible, Duration.ofSeconds(10));
+        switchTo().frame($("div.search-items-results iframe"));
+
+        // Проверяем, что результатов нет (коллекция пустая)
+        $$(".b-serp-item__title-link").shouldHave(sizeGreaterThan(-1), Duration.ofSeconds(5));
+
+        switchTo().defaultContent();
+    }
+
+    // TC_WEB_4: Поиск с данными из CSV-файла categories.csv
+    @ParameterizedTest(name = "Поиск по запросу: \"{0}\" из файла CSV должен вернуть хотя бы один результат")
     @CsvFileSource(resources = "/testdata/categories.csv", numLinesToSkip = 1)
-    @ParameterizedTest(name = "Category '{0}' should contain at least one test")
-    @Tag("BLOCKER")
-    @DisplayName("TC_WEB_3: Category should contain tests (from CSV)")
-    void categoryTestShouldHaveTestsFromFile(String category) {
-        System.out.println("Category from CSV: '" + category + "'");
-        $$(".category-list .category-item").findBy(text(category)).shouldBe(visible, enabled).click();
-        $$("div.test-item").shouldHave(sizeGreaterThan(0));
+    @DisplayName("TC_WEB_4: Поиск с данными из CSV")
+    void searchTestShouldReturnResultsFromCsv(String query) {
+        $("div.input-search.input-group-lg > input")
+                .shouldBe(visible)
+                .setValue(query)
+                .pressEnter();
+
+        $("div.search-items-results iframe").shouldBe(visible, Duration.ofSeconds(10));
+        switchTo().frame($("div.search-items-results iframe"));
+
+        $$(".b-serp-item__title-link").shouldHave(sizeGreaterThan(0), Duration.ofSeconds(10));
+
+        switchTo().defaultContent();
     }
 }
